db755b9121f426dc5bb34524a9339f507883fcc3
Phoscur <phoscur@pheelgood.net>
2019-06-15 15:21:51 +0200

    Remove typings, we only need those for non typescript code, use interfaces instead

--- typings/common/Database.d.ts
+++
@@ -1,5 +0,0 @@
-declare class Config {
-    private static connection : any;
-    public static getConnection() : object;
-    public static query(sql : string, args : object) : Promise<object>;
-}
--- typings/index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="Router.d.ts" />

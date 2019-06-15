7193cfe4a3f34088beaa92e5f93f75d649929cea
Phoscur <phoscur@pheelgood.net>
2019-06-15 16:57:08 +0200

    Fix linting (gulpfile), reorder package.json entries (scripts)

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const JSON_FILES = ["src/*.json", "src/**/*.json"];
 
 // pull in the project TypeScript config
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('scripts', () => {
-    const tsResult = tsProject.src()
-        .pipe(tsProject());
-    gulp.src(['src/config/**/*']).pipe(gulp.dest('dist/config'));
-    gulp.src(['src/schemas/**/*']).pipe(gulp.dest('dist/schemas'));
-    return tsResult.js.pipe(gulp.dest('dist'));
+gulp.task("scripts", () => {
+  const tsResult = tsProject.src().pipe(tsProject());
+  gulp.src(["src/config/**/*"]).pipe(gulp.dest("dist/config"));
+  gulp.src(["src/schemas/**/*"]).pipe(gulp.dest("dist/schemas"));
+  return tsResult.js.pipe(gulp.dest("dist"));
 });
 
-gulp.task('watch', gulp.series('scripts', () => {
-    gulp.watch(['src/**/*.ts', '!src/**/*.spec.ts'], ['scripts']);
-}));
+gulp.task(
+  "watch",
+  gulp.series("scripts", () => {
+    gulp.watch(["src/**/*.ts", "!src/**/*.spec.ts"], ["scripts"]);
+  }),
+);
 
-gulp.task('assets', function() {
-    return gulp.src(JSON_FILES)
-        .pipe(gulp.dest('dist'));
+gulp.task("assets", () => {
+  return gulp.src(JSON_FILES).pipe(gulp.dest("dist"));
 });
 
-gulp.task('default', gulp.series('watch', 'assets'));
+gulp.task("default", gulp.series("watch", "assets"));

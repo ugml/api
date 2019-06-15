52190f213c0fd7414f31bad0d15d265468a997a8
Phoscur <phoscur@pheelgood.net>
2019-06-15 18:13:11 +0200

    Add gulp-nodemon and fix/refactor gulp watch config

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,37 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
+const nodemon = require("gulp-nodemon");
+const Cache = require("gulp-file-cache");
 const JSON_FILES = ["src/*.json", "src/**/*.json"];
+const TEST_FILES = "src/**/*.spec.ts";
+const CONFIG_FILES = ["src/config/**/*"];
+const SCHEMA_FILES = ["src/schemas/**/*"];
+const SOURCE_FILES = ["src/**/*.ts", "!" + TEST_FILES];
+
+const cache = new Cache();
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("scripts", () => {
-  const tsResult = tsProject.src().pipe(tsProject());
-  gulp.src(["src/config/**/*"]).pipe(gulp.dest("dist/config"));
-  gulp.src(["src/schemas/**/*"]).pipe(gulp.dest("dist/schemas"));
+gulp.task("compile", () => {
+  const tsResult = gulp.src(SOURCE_FILES).pipe(tsProject());
   return tsResult.js.pipe(gulp.dest("dist"));
 });
 
-gulp.task(
-  "watch",
-  gulp.series("scripts", () => {
-    gulp.watch(["src/**/*.ts", "!src/**/*.spec.ts"], ["scripts"]);
-  }),
-);
-
-gulp.task("assets", () => {
-  return gulp.src(JSON_FILES).pipe(gulp.dest("dist"));
+gulp.task("watch", done => {
+  return nodemon({
+    script: "dist",
+    watch: SOURCE_FILES,
+    ext: "compile",
+    tasks: ["compile"],
+    done,
+  }).on("restart", () => console.log("Restart!"));
 });
 
-gulp.task("default", gulp.series("watch", "assets"));
+gulp.task("copy:config", () => gulp.src(CONFIG_FILES).pipe(gulp.dest("dist/config")));
+gulp.task("copy:schema", () => gulp.src(SCHEMA_FILES).pipe(gulp.dest("dist/schemas")));
+gulp.task("copy:json", () => gulp.src(JSON_FILES).pipe(gulp.dest("dist")));
+
+gulp.task("assets", gulp.series(["copy:config", "copy:schema", "copy:json"]));
+
+gulp.task("default", gulp.series("assets", "compile", "watch"));

c33964bde8fd6e94d1d697451175b21f95a1dd8f
Phoscur <phoscur@pheelgood.net>
2019-06-18 21:49:15 +0200

    Add failing test for player creation route

---
+++ src/routes/PlayerRouter.spec.ts
@@ -0,0 +1,31 @@
+import * as mocha from "mocha";
+import * as chai from "chai";
+import chaiHttp = require("chai-http");
+
+import app from "../App";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request(app);
+
+describe("Player Routes", () => {
+
+  it("should create a player", async () => {
+    const user = {
+      username: "Test",
+      password: "test",
+      email: "test",
+    };
+    return request.post("/v1/users/create/").send(user).then(res => {
+      expect(res.type).to.eql("application/json");
+      console.log("player creation response", res.body)
+    });
+  });
+
+  it.skip("should have a message prop", () => {
+    return request.get("/v1/").then(res => {
+      console.log(res.body.message);
+      expect(res.body.message).to.eql("Hello World!");
+    });
+  });
+});

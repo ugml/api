9efa8920295045485973003cd646aed923f9e482
Phoscur <phoscur@pheelgood.net>
2019-06-19 19:00:52 +0200

    Fix linting

--- src/routes/PlayerRouter.spec.ts
+++ src/routes/PlayerRouter.spec.ts
@@ -9,18 +9,20 @@ const expect = chai.expect;
 const request = chai.request(app);
 
 describe("Player Routes", () => {
-
   it("should create a player", async () => {
     const user = {
       username: "Test4",
       password: "test",
       email: "test",
     };
-    return request.post("/v1/users/create/").send(user).then(res => {
-      expect(res.type).to.eql("application/json");
-      console.log("player creation response", res.body);
-      expect(res.status).to.eql(200);
-    });
+    return request
+      .post("/v1/users/create/")
+      .send(user)
+      .then(res => {
+        expect(res.type).to.eql("application/json");
+        console.log("player creation response", res.body);
+        expect(res.status).to.eql(200);
+      });
   });
 
   it.skip("should have a message prop", () => {

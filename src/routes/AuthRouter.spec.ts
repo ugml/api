7296d172bfc43462160f0a596aef180a92885794
mamen <markusme@edu.aau.at>
2019-06-20 18:56:12 +0200

    Updated Auth-tests
    Signed-off-by: mamen <markusme@edu.aau.at>

--- src/routes/AuthRouter.spec.ts
+++ src/routes/AuthRouter.spec.ts
@@ -11,16 +11,13 @@ const request = chai.request(app);
 describe("authRoute", () => {
   // TODO properly initialize and tear down app with db connections to be able to remove mocha --exit flag!
 
-  it("should be json", async () => {
-    return request.get("/v1/auth").then(res => {
-      expect(res.type).to.eql("application/json");
-    });
+  it("should return a token", async () => {
+    return request
+      .post("/v1/auth/login")
+      .send({ email: "user_1501005189510@test.com", password: "admin" })
+      .then(res => {
+        expect(res.body.message).equals("Success");
+      });
   });
 
-  it.skip("should have a message prop", () => {
-    return request.get("/v1/").then(res => {
-      console.log(res.body.message);
-      expect(res.body.message).to.eql("Hello World!");
-    });
-  });
 });
--- src/routes/PlayersRouter.spec.ts
+++ src/routes/PlayersRouter.spec.ts
@@ -9,21 +9,21 @@ const expect = chai.expect;
 const request = chai.request(app);
 
 describe("Player Routes", () => {
-  it("should create a player", async () => {
-    const user = {
-      username: "Test4",
-      password: "test",
-      email: "test",
-    };
-    return request
-      .post("/v1/users/create/")
-      .send(user)
-      .then(res => {
-        expect(res.type).to.eql("application/json");
-        console.log("player creation response", res.body);
-        expect(res.status).to.eql(200);
-      });
-  });
+  // it("should create a player", async () => {
+  //   const user = {
+  //     username: "Test4",
+  //     password: "test",
+  //     email: "test",
+  //   };
+  //   return request
+  //     .post("/v1/users/create/")
+  //     .send(user)
+  //     .then(res => {
+  //       expect(res.type).to.eql("application/json");
+  //       console.log("player creation response", res.body);
+  //       expect(res.status).to.eql(200);
+  //     });
+  // });
 
   it.skip("should have a message prop", () => {
     return request.get("/v1/").then(res => {

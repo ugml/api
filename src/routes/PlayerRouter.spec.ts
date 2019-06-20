37c2569fa728b89742b93bfad8f86b3a3b1f1b1c
Phoscur <phoscur@pheelgood.net>
2019-06-20 12:54:13 +0200

    Add test and refactor player creation route (#44)
    * Add failing test for player creation route
    
    * Fix PlayersRouter Logger import
    
    * Refactor remove PlayersRouter init method (use constructor instead)
    
    * Refactor /users/create route with async/await
    
    * Fix linting

---
+++ src/routes/PlayerRouter.spec.ts
@@ -0,0 +1,34 @@
+import * as mocha from "mocha";
+import * as chai from "chai";
+import chaiHttp = require("chai-http");
+
+import app from "../App";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request(app);
+
+describe("Player Routes", () => {
+  it("should create a player", async () => {
+    const user = {
+      username: "Test4",
+      password: "test",
+      email: "test",
+    };
+    return request
+      .post("/v1/users/create/")
+      .send(user)
+      .then(res => {
+        expect(res.type).to.eql("application/json");
+        console.log("player creation response", res.body);
+        expect(res.status).to.eql(200);
+      });
+  });
+
+  it.skip("should have a message prop", () => {
+    return request.get("/v1/").then(res => {
+      console.log(res.body.message);
+      expect(res.body.message).to.eql("Hello World!");
+    });
+  });
+});

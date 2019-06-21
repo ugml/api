15b3c8d0e0b596719eac9b6075b435e48bc6c3d0
mamen <mamen@ugamela.org>
2019-06-21 17:04:57 +0200

    moved tests to test-folder added nyc-config
    Signed-off-by: mamen <mamen@ugamela.org>

--- test/routes/PlayersRouter.spec.ts
+++ test/routes/PlayersRouter.spec.ts
@@ -2,7 +2,7 @@ import * as mocha from "mocha";
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
 
-import app from "../App";
+import app from "../../src/App";
 
 chai.use(chaiHttp);
 const expect = chai.expect;

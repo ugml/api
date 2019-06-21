6b3e559cc562f0bf1b1d31c14690db5103e18737
mamen <mamen@ugamela.org>
2019-06-21 17:34:56 +0200

    Final fix (hopefully)
    Signed-off-by: mamen <mamen@ugamela.org>

--- src/routes/AuthRouter.spec.ts
+++ src/routes/AuthRouter.spec.ts
@@ -2,7 +2,7 @@ import * as mocha from "mocha";
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
 
-import app from "../../src/App";
+import app from "../App";
 
 chai.use(chaiHttp);
 const expect = chai.expect;

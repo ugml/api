865c5f093b4ac13d7f8d53a6f68a5ae30a3f3f86
mamen <mamen@ugamela.org>
2019-06-21 12:42:58 +0200

    Updated test
    Signed-off-by: mamen <mamen@ugamela.org>

--- src/common/QueueItem.spec.ts
+++ src/common/QueueItem.spec.ts
@@ -2,12 +2,31 @@ import * as mocha from "mocha";
 import * as chai from "chai";
 import { QueueItem } from "./QueueItem";
 
-var assert = chai.assert;
+let assert = chai.assert;
+let expect = chai.expect
 
 describe("QueueItem", function() {
   it("PlanetID should be -1", function() {
     const item: QueueItem = new QueueItem();
 
     assert.equal(item.getPlanetID(), -1);
+  });
+
+  it("LastUpdateTime should be 0", function() {
+    const item: QueueItem = new QueueItem();
+
+    assert.equal(item.getLastUpdateTime(), 0);
+  });
+
+  it("TimeRemaining should be 0", function() {
+    const item: QueueItem = new QueueItem();
+
+    assert.equal(item.getTimeRemaining(), 0);
+  });
+
+  it("Queue should be empty", function() {
+    const item: QueueItem = new QueueItem();
+
+    expect(item.getQueue()).to.be.empty;
   });
 });

7d884dd6adf9b58ae5627b4b661a2e302068cbcf
mamen <mamen@ugamela.org>
2019-12-02 07:36:26 +0100

    Added REDIS_PASSWORD
    Signed-off-by: mamen <mamen@ugamela.org>

--- src/common/Redis.ts
+++ src/common/Redis.ts
@@ -9,7 +9,11 @@ dotenv.config({
  * Manages the connection to the redis-database
  */
 export default class Redis {
-  private static client = redis.createClient(process.env.REDIS_PORT || 6379, process.env.REDIS_HOST || "localhost");
+  private static client = redis.createClient({
+    port: process.env.REDIS_PORT || 6379, // replace with your port
+    host: process.env.REDIS_HOST || "localhost", // replace with your hostanme or IP address
+    password: process.env.REDIS_PASSWORD || "", // replace with your password
+  });
   /**
    * Returns the connection-object to the redis-instance
    */
